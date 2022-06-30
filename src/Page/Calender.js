f6b3fde32e5531df0d1becbc58b563c43adb6461
Md Masum Rana <masummx3@gmail.com>
2022-06-30 22:23:11 +0600

    add calendar

--- src/Page/Calender.js
+++ src/Page/Calender.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
+import 'react-day-picker/dist/style.css';
 
 
 const Calender = () => {
+    const [selected, setSelected] = useState(new Date());
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You have picked: {format(selected, 'PP')}.</p>;
+      }
 
-    return (
-        <div>
-          
-
-        </div>
-    );
+    return( <DayPicker 
+        className="center"
+    mode="single"
+    selected={selected}
+    onSelect={setSelected}
+    footer={footer}
+    />);
 };
 
 export default Calender;
